fc10bbecf469b2f943fbf6de9d93425cbf0c62b6
Jonathan <info@jonathan-wilke.de>
2025-09-04 16:40:26 +0200

    fix: add icon.png to middleware ignore

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -61,6 +61,6 @@ export default async function middleware(req: NextRequest) {
 
 export const config = {
 	matcher: [
-		"/((?!api|image-proxy|images|fonts|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+		"/((?!api|image-proxy|images|fonts|_next/static|_next/image|favicon.ico|icon.png|sitemap.xml|robots.txt).*)",
 	],
 };
