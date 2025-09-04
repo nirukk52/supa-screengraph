f723d488751013c83a84a6aef1b55c0802efc5ce
Jonathan <info@jonathan-wilke.de>
2025-09-04 09:02:37 +0200

    chore: remove unused jotai

--- apps/web/modules/shared/components/ClientProviders.tsx
+++ apps/web/modules/shared/components/ClientProviders.tsx
@@ -6,7 +6,6 @@ import { config } from "@repo/config";
 import { ApiClientProvider } from "@shared/components/ApiClientProvider";
 import { ConsentBanner } from "@shared/components/ConsentBanner";
 import { Toaster } from "@ui/components/toast";
-import { Provider as JotaiProvider } from "jotai";
 import { ThemeProvider } from "next-themes";
 import type { PropsWithChildren } from "react";
 
@@ -28,13 +27,11 @@ export function ClientProviders({ children }: PropsWithChildren) {
 					themes={config.ui.enabledThemes}
 				>
 					<ApiClientProvider>
-						<JotaiProvider>
-							{children}
+						{children}
 
-							<Toaster position="top-right" />
-							<ConsentBanner />
-							<AnalyticsScript />
-						</JotaiProvider>
+						<Toaster position="top-right" />
+						<ConsentBanner />
+						<AnalyticsScript />
 					</ApiClientProvider>
 				</ThemeProvider>
 			</ProgressProvider>
